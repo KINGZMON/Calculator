8aa5fe04e70973babd8090d6f59988f1aee0f1c3
KINGZMON <myf11302@gmail.com>
2022-12-13 20:42:58 +0200

    Added Unit tests

--- app/src/test/java/com/example/calculator/ExampleUnitTest.java
+++ app/src/test/java/com/example/calculator/ExampleUnitTest.java
@@ -1,6 +1,7 @@
 package com.example.calculator;
 
 import org.junit.Test;
+import org.mariuszgromada.math.mxparser.Expression;
 
 import static org.junit.Assert.*;
 
@@ -11,7 +12,44 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void Test01Calculation() {
+        String inputExp = "1+2";
+        Expression exp = new Expression(inputExp);
+
+        String expectedValue = "3.0";
+        String actualValue = String.valueOf(exp.calculate());
+
+        assertEquals(expectedValue, actualValue);
     }
+
+    @Test
+    public void Test02Calculation() {
+        String inputExp = "1+2/3*4";
+        Expression exp = new Expression(inputExp);
+
+        String expectedValue = "3.6666666666666665";
+        String actualValue = String.valueOf(exp.calculate());
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test
+    public void Test03Calculation() {
+        String inputExp = "9^2";
+        Expression exp = new Expression(inputExp);
+
+        String expectedValue = "81.0";
+        String actualValue = String.valueOf(exp.calculate());
+
+        assertEquals(expectedValue, actualValue);
+    }
+
+    @Test (expected = NullPointerException.class)
+    public void Test04Calculation() {
+        String inputExp = null;
+        Expression exp = new Expression(inputExp);
+        String actualValue = String.valueOf(exp.calculate());
+    }
+
+
 }
